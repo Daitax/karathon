fe73296463f92602855b04af427d1201964e0a1f
Daitax <lars1515@yandex.ru>
2022-11-10 16:49:45 +0400

    Создание в отчётах папки пользователя и отображение отчётов в админке

--- apps/steps/admin.py
+++ apps/steps/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.steps.models import Step
+
+
+class StepAdmin(admin.ModelAdmin):
+    list_display = ('participant', 'date', 'steps', 'photo_preview_in_list')
+
+    fields = ('participant', 'date', 'steps', 'photo_preview',)
+    readonly_fields = ('participant', 'date', 'photo_preview',)
+    search_fields = ('participant__first_name', 'participant__last_name',)
+    search_help_text = 'Для поиска введите имя или фамилию участника'
+
+
+admin.site.register(Step, StepAdmin)
