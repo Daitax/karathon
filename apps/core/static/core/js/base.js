905f9e9a84c9f37ff4b98093e077686fd2012d7c
AlexeyKondrukevich <kondr08@mail.ru>
2022-11-16 10:30:14 +0400

    added sticky navbar

--- apps/core/static/core/js/base.js
+++ apps/core/static/core/js/base.js
@@ -2,11 +2,11 @@ function closePopup(event) {
   let overlay = document.querySelector('[popup-element="overlay"]')
   let popup = document.querySelectorAll('[popup-element="popup"]')
 
-  if(event.target == overlay) {
-    if(overlay.querySelector('[reload="True"]')){
+  if (event.target == overlay) {
+    if (overlay.querySelector('[reload="True"]')) {
       window.location.reload()
     } else {
-      popup.forEach(function (element){
+      popup.forEach(function (element) {
         element.classList.remove('show')
       })
       overlay.classList.remove('show')
--- apps/core/static/core/js/functions.js
+++ apps/core/static/core/js/functions.js
@@ -4,3 +4,42 @@ function getCookie(name) {
   ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
+
+
+let stickyNavbar = document.querySelector('[window-elem="sticky_navbar"]')
+let firstScreen = document.querySelector('[window-elem="first-screen"]')
+let firstScreenBottom = firstScreen.getBoundingClientRect().bottom
+let leftOffset = firstScreen.getBoundingClientRect().left / 2 + "px"
+let stickyNavbarStyle = "position: fixed; left: " + leftOffset + "; padding: 10px 0 10px " + leftOffset + "; z-index: 2;"
+let footer = document.querySelector('[window-elem="footer"]')
+let footerTop = footer.offsetTop
+
+let stickyAccMenu = document.querySelector('[window-elem="sticky_navbar"] > [window-elem="account_menu"]')
+// console.log(stickyAccMenu)
+
+window.addEventListener("scroll", function () {
+  let screenTop = window.pageYOffset
+
+  if ((firstScreenBottom <= screenTop) && (screenTop <= footerTop)) {
+    stickyNavbar.setAttribute("style", stickyNavbarStyle + "top: 0")
+    stickyAccMenu.setAttribute("style", "display: block; transition: 1s")
+  } else {
+    stickyNavbar.setAttribute("style", stickyNavbarStyle + "top: -50px")
+    stickyAccMenu.setAttribute("style", "display: none")
+  }
+})
+
+// let header = document.querySelector('[window-elem="header"]')
+// let navbar = document.querySelector('[window-elem="navbar"]')
+// let leftOffset = navbar.getBoundingClientRect().left / 2 + "px"
+// let footer = document.querySelector('[window-elem="footer"]')
+
+// window.addEventListener("scroll", function () {
+//   let screenTop = window.pageYOffset
+//   let footerTop = footer.offsetTop
+//   let headerHeight = header.offsetHeight
+//   if (screenTop <= footerTop) {
+//     navbar.setAttribute("style", "position: fixed; left: " + leftOffset + "; padding: 10px 0 10px " + leftOffset + "; background: #F648A5; margin-top: 10px; z-index: 1;")
+//     header.setAttribute("style", "min-height: " + headerHeight + "px")
+//   }
+// })
