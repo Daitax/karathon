d93c6ed649beb4ada9b4e705a732dd09489e8c71
Daitax <lars1515@yandex.ru>
2022-12-08 14:26:01 +0400

    Правки

--- apps/account/context_processors/report_sent.py
+++ apps/account/context_processors/report_sent.py
@@ -1,3 +1,14 @@
 def report_sent(request):
-    report_sent = request.user.participant.is_today_report()
-    return {"report_sent": report_sent}
+    if request.user.is_authenticated:
+        report_sent = request.user.participant.is_today_report()
+        return {"report_sent": report_sent}
+    else:
+        return {"report_sent": False}
+
+
+def active_karathon(request):
+    if request.user.is_authenticated:
+        active_karathon = request.user.participant.get_active_karathon()
+        return {"active_karathon": active_karathon}
+    else:
+        return {"active_karathon": False}
--- project/settings.py
+++ project/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'apps.account.context_processors.report_sent.report_sent',
+                'apps.account.context_processors.report_sent.active_karathon',
             ],
         },
     },
